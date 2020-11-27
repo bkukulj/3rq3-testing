9af2cdeb3b940ffeb0516343789701edb3abeb94
Sean Watson <sean.watson@uhn.ca>
2020-11-26 20:58:48 -0500

    adding price

--- dinnerisserved/menuitem.py
+++ dinnerisserved/menuitem.py
@@ -2,6 +2,7 @@ class MenuItem:
     def __init__(self, name):
         self.name = name
         self.valid = True
+        self.price = 1.5
 
     def invalidate_item(self):
         self.valid = False
